3afe6cd842a8b0a14deae1c6553680738cbe5f49
Mohinishinde1418 <156390988+Mohinishinde1418@users.noreply.github.com>
2025-03-18 17:06:18 +0530

    create a forget pag

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,18 @@
+import { ForgotPassComponent } from './auth/forgot-pass/forgot-pass.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
 
 const routes: Routes = [
+  // {
+  //   path: '', component: LoginComponent
+  // },
+  // {
+  //   path: 'dashboard', component: DashboardComponent
+  // },
   {
-    path: '', component: LoginComponent
-  },
-  {
-    path: 'dashboard', component: DashboardComponent
+    path: '', component: ForgotPassComponent
   }
 ];
 
