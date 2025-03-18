28efdd83b1abe8e175114c8fd2b05015ccea92c8
aniketmehatre <aniketmehatre@gmail.com>
2025-03-18 11:20:52 +0530

    create foluder stur

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { ForgotPassComponent } from './auth/forgot-pass/forgot-pass.component';
+import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
+import { AddTaskComponent } from './dashboard/add-task/add-task.component';
+import { ViewTaskComponent } from './dashboard/view-task/view-task.component';
+import { TaskDetailComponent } from './dashboard/task-detail/task-detail.component';
+import { AddUserComponent } from './dashboard/add-user/add-user.component';
+import { ViewUserComponent } from './dashboard/view-user/view-user.component';
+import { AddProjectComponent } from './dashboard/add-project/add-project.component';
+import { ViewProjectComponent } from './dashboard/view-project/view-project.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ForgotPassComponent,
+    DashboardComponent,
+    AddTaskComponent,
+    ViewTaskComponent,
+    TaskDetailComponent,
+    AddUserComponent,
+    ViewUserComponent,
+    AddProjectComponent,
+    ViewProjectComponent
   ],
   imports: [
     BrowserModule,
